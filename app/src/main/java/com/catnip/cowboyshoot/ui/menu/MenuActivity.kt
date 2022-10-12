249b0bc432a805c40ff73eeb652d7163c8822151
ajengrf12 <ajengrf12@gmail.com>
2022-10-12 08:53:53 +0700

    add onclick listener

--- app/src/main/java/com/catnip/cowboyshoot/ui/menu/MenuActivity.kt
+++ app/src/main/java/com/catnip/cowboyshoot/ui/menu/MenuActivity.kt
@@ -1,9 +1,13 @@
 package com.catnip.cowboyshoot.ui.menu
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.core.content.ContentProviderCompat.requireContext
 import com.catnip.cowboyshoot.R
 import com.catnip.cowboyshoot.databinding.ActivityMenuBinding
+import com.catnip.cowboyshoot.ui.game.GameActivity
 
 class MenuActivity : AppCompatActivity() {
     private val binding: ActivityMenuBinding by lazy {
@@ -15,11 +19,30 @@ class MenuActivity : AppCompatActivity() {
         setContentView(binding.root)
         supportActionBar?.hide()
         getUserName()
+        setOnClickListener()
     }
 
     private fun getUserName() {
         val username = intent.getStringExtra("username")
         binding.tvTitle.text =
             getString(R.string.text_hi) + username + getString(R.string.text_exclamation_mark)
+    }
+
+    private fun setOnClickListener() {
+        binding.ivVersusComputer.setOnClickListener {
+            playWithComputer()
+        }
+        binding.ivVersusPlayer.setOnClickListener {
+            playWithAnotherPlayer()
+        }
+    }
+
+    private fun playWithComputer() {
+        val intent = Intent(this@MenuActivity, GameActivity::class.java)
+        startActivity(intent)
+    }
+
+    private fun playWithAnotherPlayer() {
+        Toast.makeText(applicationContext, getString(R.string.text_coming_soon), Toast.LENGTH_SHORT).show()
     }
 }
